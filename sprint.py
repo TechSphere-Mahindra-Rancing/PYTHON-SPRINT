17eeaeb3aba9b8601279f4999bcdd704fe0c5780
Jean <jeanmatheus0612@gmail.com>
2024-09-19 17:39:33 -0300

    Criando a função de boas vindas

--- sprint.py
+++ sprint.py
@@ -1,3 +1,15 @@
 import re
 import hashlib
 
+# Constante para separação visual
+BARRA = "-" * 100
+
+# Armazenamento de dados (dicionário para usuários)
+usuarios = {}
+
+# Função para exibir mensagem de boas-vindas
+def boas_vindas():
+    """Exibe mensagem de boas-vindas."""
+    print(BARRA)
+    print("\nMahindra")
+    print("Seja bem vindo(a)!\n")
